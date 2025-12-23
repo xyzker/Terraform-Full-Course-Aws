bfdb261480b8679267cc6a8b268f58eaff85b819
xyzker <410317669@qq.com>
2025-12-23 11:47:23 +0800

    update

--- lessons/day14/backend.tf
+++ lessons/day14/backend.tf
@@ -1,7 +1,8 @@
-terraform {
+/*terraform {
   backend "s3" {
     bucket = "my-terraform-state-bucket-piyushsachdeva"
     key    = "lessons/day14/terraform.tfstate"
     region = "us-east-1"
   }
 }
+*/
